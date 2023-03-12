fd9e3b4c67a1613a3fae40866c73dc2a4f69db87
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-03-12 12:51:05 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import Routes from './routes'
 import './App.scss'
+import { useNavigate } from 'react-router-dom'
 
 const App = () => {
+
+  const tableId = localStorage.getItem('tableId')
+
+  const Navigate = useNavigate()
+
+  React.useEffect(() => {
+    Navigate(tableId)
+  }, [])
 
   setInterval(() => {
     window.location.reload()
