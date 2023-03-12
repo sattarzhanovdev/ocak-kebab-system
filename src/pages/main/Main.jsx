2c52feb094b841f64c98666d37cc42c8de5d5888
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-03-12 13:14:54 +0600

    finish

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -9,13 +9,11 @@ const Main = () => {
   const basketId = localStorage.getItem('basketId')
 
   React.useEffect(() => {
-    // localStorage.setItem('tableId', tableId)
-    // if(!basketId){
-    //   REQUEST.postBaskets({qrcode_id: tableId})
-    //     .then(res => localStorage.setItem('basketId', res.data.id))
-    // }
-  localStorage.clear()
-    
+    localStorage.setItem('tableId', tableId)
+    if(!basketId){
+      REQUEST.postBaskets({qrcode_id: tableId})
+        .then(res => localStorage.setItem('basketId', res.data.id))
+    }
   }, [tableId])
 
   return (
